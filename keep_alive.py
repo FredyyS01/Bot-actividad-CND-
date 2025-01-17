4d471b0909093000bbd08007e4ba17676c52526a
FredyyS01 <fredyy01games@gmail.com>
2025-01-17 12:22:33 -0500

    keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -5,11 +5,11 @@ app = Flask('')
 
 @app.route('/')
 def home():
-    return "¡Bot está en línea!"
+    return "Bot Activo!"
 
 def run():
     app.run(host='0.0.0.0', port=8080)
 
-def keep_alive():
-    t = Thread(target=run)
-    t.start()
+def keep_alive():    
+    server = Thread(target=run)
+    server.start()
